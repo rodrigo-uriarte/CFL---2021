999d8a1454e10527652bfee8551bbb7135ae9131
rodrigo-uriarte <orj.uriarte@gmail>
2021-04-26 17:24:37 -0300

    Ejercicios clase 2

---
+++ Modulo 1 - Programación básica/Clase 2/AlgoritmoSecuencial.js
@@ -0,0 +1,4 @@
+console.log("este");
+console.log("algoritmo");
+console.log("es");
+console.log("secuencial");
---
+++ Modulo 1 - Programación básica/Clase 2/Altura.js
@@ -0,0 +1,3 @@
+let readlineSync = require("readline-sync");
+let alturaUsuario = readlineSync.question("cual es su altura  ");
+console.log("su altura es", alturaUsuario);
